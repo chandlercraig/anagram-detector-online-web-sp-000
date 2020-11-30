09584472ca5214757d21d69f9daf96154d09c68c
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-30 20:04:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,8 +6,10 @@ class Anagram
   end
 
   def match(anarray)
-    anarray.each do |word|
-      word.split("")
+    anarray.each do |some_word|
+      word_parse_arr = some_word.split("")
+      if word_parse_arr.sort ==
+
   end
 
 end
