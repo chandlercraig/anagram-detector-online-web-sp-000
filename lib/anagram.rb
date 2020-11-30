d5306d80af47ad7b4f6399396a2e4c9ce0dc359c
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-30 20:24:01 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ origin_arr = @word.split("").sort
 anarray.each do |some_word|
 word_parse_arr = some_word.split("").sort
 if word_parse_arr == origin_arr
-some_word
+return some_word
 end
 end
 end
