6ef44ab8b56cb257548ff206812040cd84a00212
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-30 20:06:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,9 +6,13 @@ class Anagram
   end
 
   def match(anarray)
+    origin_word_arr = @word.split("")
     anarray.each do |some_word|
       word_parse_arr = some_word.split("")
-      if word_parse_arr.sort ==
+      if word_parse_arr.sort == origin_word_arr.sort
+        some_word
+      else
+        []
 
   end
 
